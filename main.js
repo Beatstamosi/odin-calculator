8580c29fc7aa550049265ac4c00f92997d92e3b0
Beatstamosi <moritz.lued@gmail.com>
2024-11-25 12:03:35 -0600

    Add HTML Structure & CSS Styling

--- main.js
+++ main.js
@@ -14,7 +14,31 @@ function divide(number, number2) {
     if (number2 === 0) {
         return "ERROR";
     }
-    
     return number / number2;
 }
+
+
+function percentage (number) {
+    return number / 100;
+}
+
+let number;
+let operator;
+let number2;
+
+function operate(number, operator, number2) {
+    if (operator === "+") {
+        add(number, number2);
+    } else if (operator === "-") {
+        subtract(number, number2);
+    } else if (operator === "/") {
+        divide(number, number2);
+    } else if (operator === "+") {
+        multiply(number, number2);
+    } else if (operator === "%") {
+        percentage(number);
+    }
+}
+
+// 
 
